68ad59faff5ec67617ab1e1801024f555f375c62
weezykym <mcweezy49@gmail.com>
2026-02-26 13:18:56 +0300

    Cross Origin url

--- src/main/java/com/kevin/tasks/TasksApplication.java
+++ src/main/java/com/kevin/tasks/TasksApplication.java
@@ -2,7 +2,9 @@ package com.kevin.tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "https://task-tracker-app-ui.up.railway.app")
 @SpringBootApplication
 public class TasksApplication {
 
